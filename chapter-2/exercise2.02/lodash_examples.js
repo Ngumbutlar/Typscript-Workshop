ac62ec198052464ea629ac90161ecfb7e41f25a8
Ngum <ngumbutlar43@gmail.com>
2023-08-18 14:52:40 +0100

    Creating types with external libaries

---
+++ chapter-2/exercise2.02/lodash_examples.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _ = require("lodash");
+const nums = [1, 2, 3];
+console.log(_.last(nums));
+// const number = 10;
+// console.log(_.last(number));
