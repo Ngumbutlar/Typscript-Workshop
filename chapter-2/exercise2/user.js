aca8f1b43860a54432c121e98ea19ab23d24e4d7
Ngum <ngumbutlar43@gmail.com>
2023-08-18 14:51:31 +0100

    Created a declaration file from scratch.

---
+++ chapter-2/exercise2/user.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let jon;
+jon = {
+    email: "jon@snow.com",
+    roles: ["admin"]
+};
+let alice;
+alice = {
+    email: "alice@snow.com",
+    roles: ["super_admin"],
+    source: "facebook"
+};
