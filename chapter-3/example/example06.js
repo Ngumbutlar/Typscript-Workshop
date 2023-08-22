f41a018282684dc596cc7e7a2edadb410d22208b
Ngum <ngumbutlar43@gmail.com>
2023-08-22 04:37:30 +0100

    (CH-3) Examples

---
+++ chapter-3/example/example06.js
@@ -0,0 +1,19 @@
+"use strict";
+// Rest Parameters ... Example 06
+// function readBooks(title:string, ...chapters: number[]): void{
+//     console.log(`Starting to rea ${title}...`);
+//     chapters.forEach(chapter => {
+//         console.log(`Reading chapter ${chapter}.`);
+//     });
+//     console.log('Done reading.');
+// }
+// readBook('The Typescript Workshop', 1, 2, 3);
+// Example07
+function readBooks(title, chapters) {
+    console.log(`Starting to read ${title}...`);
+    chapters.forEach(chapter => {
+        console.log(`Reading chapter ${chapter}.`);
+    });
+    console.log('Done reading.');
+}
+readBooks('The Typescript Book', [1, 2, 3]);
