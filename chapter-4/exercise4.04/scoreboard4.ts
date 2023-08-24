5699047db6591d59ddca03bc7830e2e72e781d0a
Ngum <ngumbutlar43@gmail.com>
2023-08-24 13:40:30 +0100

    (CH-4) Building an interface

---
+++ chapter-4/exercise4.04/scoreboard4.ts
@@ -0,0 +1,36 @@
+interface ITeam{
+    name: string;
+    players: string[];
+}
+
+
+class Team4{
+   
+    name: string;
+    players: string[];
+
+    constructor(args: ITeam){
+        this.name = args.name;
+        this.players = args.players;
+    }
+    generateLineUp(){
+        return this.players.join(", ");
+    }
+}
+
+const astroPlayers2 = ["Altuve", "Bregman", "Correa", "Springer"];
+const astros4 = new Team4({
+    name: "Astros",
+    players: astroPlayers2
+});
+console.log(astros4.generateLineUp());
+console.log(astros4.name);
+
+const blueJaysPlayer2 = ["Vlad", "Smoak", "Teller", "Sogard"];
+const blueJays2 = new Team4({
+    name: "Blue Jays",
+    players: blueJaysPlayer2
+});
+console.log(blueJays2.generateLineUp());
+console.log(blueJays2.name);
+
