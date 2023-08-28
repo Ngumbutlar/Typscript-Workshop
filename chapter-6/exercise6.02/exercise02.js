34136ddb02f0d1b5b0becebb0caba8a6bcaa167a
Ngum <ngumbutlar43@gmail.com>
2023-08-28 03:19:39 +0100

    (CH-6) Type Literals

---
+++ chapter-6/exercise6.02/exercise02.js
@@ -0,0 +1,6 @@
+"use strict";
+//function tha ttakes a string literal and returns a number literal
+function onlyNo(no) {
+    return 0;
+}
+console.log(onlyNo("no"));
---
+++ chapter-6/exercise6.02/exercise02.ts
@@ -0,0 +1,9 @@
+//function tha ttakes a string literal and returns a number literal
+
+type No = "no";
+type Zero = 0;
+function onlyNo(no: No): Zero{
+    return 0;
+}
+
+console.log(onlyNo("no"));
